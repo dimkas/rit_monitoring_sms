dba1e8886ce40ea6c7eb8493e82ead3f0251c400
Dima Shekin <dshekin@gmail.com>
2014-04-26 00:01:46 +0300

    Initial commit

---
+++ app/src/main/java/ru/ookamilb/rugball/UL.java
@@ -0,0 +1,37 @@
+package ru.ookamilb.rugball;
+
+
+/**
+ * Created by dimkas on 25.04.14.
+ */
+public class UL {
+    String header;
+    String time;
+    String version;
+    String someData1;
+    String voltage;
+    String temperature;
+    String someData2;
+    String firstMeeterValue;
+    String secondMeeterValue;
+    String someData3;
+
+    public UL(String ul) {
+        header = ul.substring(0, 2);
+        time = ul.substring(3, 11);
+        version = ul.substring(13, 16);
+        someData1 = ul.substring(18, 51);
+        voltage = ul.substring(52, 69);
+        temperature = ul.substring(70, 71);
+        someData2 = ul.substring(72, 78);
+        firstMeeterValue = ul.substring(79, 89);
+        secondMeeterValue = ul.substring(90, 100);
+        someData3 = ul.substring(101);
+    }
+
+    @Override
+    public String toString() {
+        String tmp = "Version: " + version + "\n Voltage: " + voltage + "\n Temperature: " + temperature;
+        return tmp;
+    }
+}
